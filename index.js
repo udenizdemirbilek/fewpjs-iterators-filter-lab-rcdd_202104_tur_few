786e0e4443a2d4f6fe84d8ddf0361f74642218b1
demirbileku <udemirbilek2@gmail.com>
2021-05-16 01:13:12 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching (driverArray, string){
+    const upperCaseString = string.charAt(0).toUpperCase () + string.slice(1);
+	// console.log(upperCaseString)
+    //  const upperCaseDriverArray = driverArray.map(driver => driver.charAt(0).toUpperCase() + driver.slice(1) )
+    // console.log(upperCaseDriverArray)
+   return driverArray.filter(name => (name.charAt(0).toUpperCase() + name.slice(1)) === upperCaseString)
+}
+//Letter check for every letter in string 
+function fuzzyMatch (driverArray, string) {
+    //Beyin bedava kelime başında string olacak şekilde RegEXP
+    const regexp = new RegExp("(^" + string +")", 'i');
+    return driverArray.filter(driver => regexp.test(driver))
+}
+
+function matchName (driverArray, string) {
+return driverArray.filter(driver => driver.name === string)
+}
